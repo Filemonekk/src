18c5160c62748c301020e808b4afd2c98375f791
KasiaH <kah.gda@wp.pl>
2024-09-10 13:38:15 +0200

    work

---
+++ main.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+const rootElement = document.getElementById("root")!;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
