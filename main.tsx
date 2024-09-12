c574828a82d8e8b041125e07c2d1948cbaeb8697
KasiaH <kah.gda@wp.pl>
2024-09-12 14:29:23 +0200

    modified application

--- main.tsx
+++ main.tsx
@@ -1,12 +1,10 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const rootElement = document.getElementById("root")!;
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
--- ui/Text/Text.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {
-    children: string;
-}
-
-export const Text = ({children }: Props) => {
-    return <p>{children}</p>;
-};
