8e9a7e21de01434a60685d33e1865d1662de2779
KasiaH <kah.gda@wp.pl>
2024-09-12 11:02:08 +0200

    merging branches

--- ui/Button/Button.tsx
+++ ui/Button/Button.tsx
@@ -1,10 +1,17 @@
-type Props = {
-    label: string;
-    onClick: () => void
-};
+import { type MouseEventHandler, type ComponentProps } from "react";
 
-export const Button = ({ label, onClick }: Props) => {
-    return (
-        <button onClick={onClick} className="px-4 py-1 text-sm text-white bg-blue-600 rounded-full border border-blue-900">{label}</button>
-    );
+type Props = {
+  label: string;
+  // onClick: MouseEventHandler<HTMLButtonElement>;
+} & ComponentProps<"button">;
+
+export const Button = ({ label, ...rest }: Props) => {
+  return (
+    <button
+      className="px-4 py-1 text-sm text-white bg-blue-600 rounded-sm border cursor-pointer border-blue-200 hover:bg-blue-500 disabled:bg-slate-400"
+      {...rest}
+    >
+      {label}
+    </button>
+  );
 };
